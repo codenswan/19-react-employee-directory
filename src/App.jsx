c8fd436141aa976444640a7eaa73f1f360ded458
Dan <rodger.swan@gmail.com>
2020-09-30 19:38:32 +1000

    search on input feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,78 @@
 import React, { useState, useEffect } from "react";
+import { Container, Jumbotron } from "react-bootstrap";
 import API from "./Utils/API";
 import "./App.css";
 import StaffTable from "./components/StaffTable";
+import Searchbox from "./components/Searchbox";
 
 export default function App() {
-  const [staffList, setStaffList] = useState();
-  const [fitleredStaff, setFitleredStaff] = useState();
-  const [userInput, setUserInput] = useState();
+  const [staffList, setStaffList] = useState([]);
+  const [filteredStaff, setFilteredStaff] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [sortColumn, setSortColumn] = useState();
 
   useEffect(() => {
     API.search()
       .then((response) => {
+        setStaffList(response.data.results);
         let formattedStaff = response.data.results.map((staff) => {
           return {
-            pic: staff.picture.medium,
-            id: staff.login.username,
-            name: `${staff.name.first} ${staff.name.last}`,
-            email: staff.email,
-            phone: staff.phone,
+            Photo: staff.picture.medium,
+            Name: `${staff.name.first} ${staff.name.last}`,
+            Username: staff.login.username,
+            Age: staff.dob.age,
+            Email: staff.email,
+            Phone: staff.phone,
+            City: staff.location.city,
+            Country: staff.location.country
           };
         });
-        setStaffList(formattedStaff);
-        console.log(formattedStaff);
-        setFitleredStaff(formattedStaff);
+        setFilteredStaff(formattedStaff);
       })
 
       .catch((error) => console.log(error));
   }, []);
 
+  function handleInputChange(event) {
+    setUserInput(event.target.value);
+  }
+
+  function search(rows) {
+    let searchResults = rows.filter(
+      (row) =>
+        row.Name.toLowerCase().indexOf(userInput) > -1 ||
+        row.Username.toLowerCase().indexOf(userInput) > -1 ||
+        row.Email.toLowerCase().indexOf(userInput) > -1 ||
+        row.City.toLowerCase().indexOf(userInput) > -1 ||
+        row.Country.toLowerCase().indexOf(userInput) > -1
+    ) 
+
+    if (sortColumn === "Username") {
+      searchResults.sort((a, b) => {
+        return a.Name < b.Name ? -1 : 1;
+      });
+    }
+    return searchResults;
+  }
+
+  function onColumnClick(heading) {
+    console.log(heading);
+    setSortColumn(heading)
+  }
+
   return (
-    <div>
-      <div>filter goes here</div>
-      <StaffTable/>
-    </div>
+    <Container fluid>
+      <Jumbotron
+        className="d-flex justify-content-center"
+        style={{ backgroundColor: "#7AC9ED" }}
+      >
+        <h1>Staff Directory</h1>
+      </Jumbotron>
+      <Searchbox userInput={userInput} handleInputChange={handleInputChange} />
+      <StaffTable
+        onColumnClick={onColumnClick}
+        staffList={search(filteredStaff)}
+      />
+    </Container>
   );
 }
