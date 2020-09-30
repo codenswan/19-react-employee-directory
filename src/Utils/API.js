cec106e826394f266e5972988e1842959c6a7e5b
Dan <rodger.swan@gmail.com>
2020-09-30 19:36:59 +1000

    increase api limit

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const randomUrl = "https://randomuser.me/api/?results=100";
+const randomUrl = "https://randomuser.me/api/?results=500";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
