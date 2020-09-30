88bf6898ece7ae0a330a768a5d2cf4fdc1e4b027
Dan <rodger.swan@gmail.com>
2020-09-30 19:38:01 +1000

    stafftable feature; renders data

---
+++ src/components/StaffTable.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, Table } from "react-bootstrap";
+
+export default function StaffTable(props) {
+  let headings = [];
+  if (props.staffList && props.staffList.length > 0) {
+    headings = Object.keys(props.staffList[0]).map((heading) => {
+      return <th key={heading}>{heading}</th>;
+    });
+  }
+
+  let rows;
+  if (props.staffList) {
+    rows = props.staffList.map((staff) => {
+      const tds = [];
+      for (const key in staff) {
+        const value = staff[key];
+        if (key === "Photo") {
+          tds.push(
+            <td key={value}>
+              <img src={value} alt={value} />
+            </td>
+          );
+        } else {
+          tds.push(<td key={value}>{value}</td>);
+        }
+      }
+      return <tr key={staff.Username}>{tds}</tr>;
+    });
+  } else {
+    rows = "Loading...";
+  }
+
+  return (
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>{headings}</tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </Container>
+  );
+}
